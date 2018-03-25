0dbb9f65ccde87f945bf4d5cd5c8b316dfc803cf
Thánh Ngữa <huynhduc765@gmail.com>
2018-03-26 00:34:08 +0700

    de phong bao hanh

---
+++ ShopHaiSan/Controllers/DefaultController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShopHaiSan.Controllers
+{
+    
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        ShopHaiSanOnlineEntities db = new ShopHaiSanOnlineEntities();
+        public ActionResult TrangChu()
+        {
+            return View();
+        }
+        public ActionResult getMenu()
+        {
+            var v = from t in db.MENUs
+                    select t;
+            return PartialView(v.ToList());
+        }
+    }
+}
